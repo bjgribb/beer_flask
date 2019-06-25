a09a05d9077e5a4875d9277cb574f2d8b399e351
bjgribb <bjgribb@gmail.com>
2019-06-25 10:30:09 -0400

    added templates and base.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello, World!'
+    user = {'username': 'Brian'}
+    posts = [
+        {
+            'author': {'username': 'Sarah'},
+            'body': 'Beautiful'
+        },
+        {
+            'author': {'username': 'Joelle'},
+            'body': 'Design Guru'
+        }
+    ]
+    return render_template('home.html', user=user, posts=posts)
