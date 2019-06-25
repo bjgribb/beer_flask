a9c11565ac553de6f3a47a3d3688b31dec4e6794
bjgribb <bjgribb@gmail.com>
2019-06-25 11:04:17 -0400

    further form validation and added url_for instead of hardcoded endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from app import app
 
@@ -18,7 +18,11 @@ def home():
     ]
     return render_template('home.html', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
     return render_template('login.html', title='Sign In', form=form)
