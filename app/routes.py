1cdc9d2948e97836ae0cd90743fe3411ee7075ba
bjgribb <bjgribb@gmail.com>
2019-06-26 10:25:51 -0400

    user profile page setup

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,14 @@
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm
 from app import app, db
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-    if not current_user.is_authenticated:
-        return redirect(url_for('login'))
     posts = [
         {
             'author': {'username': 'Sarah'},
@@ -57,3 +56,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
