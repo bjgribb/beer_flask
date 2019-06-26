95124ffede4cf173b936c82354f4817d8483a41e
bjgribb <bjgribb@gmail.com>
2019-06-25 20:02:34 -0400

    user login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
 from app import app
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/home')
 def home():
-    user = {'username': 'Brian'}
     posts = [
         {
             'author': {'username': 'Sarah'},
@@ -16,13 +18,26 @@ def home():
             'body': 'Design Guru'
         }
     ]
-    return render_template('home.html', user=user, posts=posts)
+    return render_template('home.html', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
