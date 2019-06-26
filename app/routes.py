e8d85963a8c7952656ee1efec42801b2b5a748c0
bjgribb <bjgribb@gmail.com>
2019-06-26 10:14:33 -0400

    finished registration for new users

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
-from app import app
+from app.forms import LoginForm, RegistrationForm
+from app import app, db
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
@@ -8,6 +8,8 @@ from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/home')
 def home():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     posts = [
         {
             'author': {'username': 'Sarah'},
@@ -41,3 +43,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
